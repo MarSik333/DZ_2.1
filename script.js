23146c368992141e1eb433d6a023cd57c44102fc
MarSik333 <ramiras333@gmail.com>
2023-02-28 18:46:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let Name = prompt("Як вас звати?");
+alert('Hello '+ Name);
+let Age = prompt("Твій рік народження?");
+alert(2023 - Age);
+let Perimeter = prompt("Яка довжина сторони квадрату? Я покажу периметр");
+alert(Perimeter * 4);
+let Radius = prompt("Радіус кола . . .")
+alert(Radius * Radius * Math.PI);
+let Distansion = prompt("Яка відстань в км, між мустами?");
+let Time = prompt("Скільки в вас є годин?")
+let Speed = (Distansion / Time)
+alert('Швидкість з якою потрібно буде рухатись.' +  Speed + 'км/ч');
+let ConvertorInEuro = prompt("Введіть валюту в $");
+let Euro = ConvertorInEuro * 0.94;
+alert(ConvertorInEuro + 'USD в EURO = '+ Euro);
